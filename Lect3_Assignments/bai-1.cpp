a21d9a4ad31c0ed19ede105fa33ecdc58d574e33
23021456 <23021456@vnu.edu.vn>
2025-03-08 00:12:30 +0700

    Create bai-1.cpp

---
+++ Lect3_Assignments/bai-1.cpp
@@ -0,0 +1,8 @@
+/*  O(1) :      2^10
+ O(n) :         2^log n, 3n + 100 log n, 4n
+ O(n log n) :   4n log n + 2n, n log n
+ O(n^2) :       n^2 + 10n
+ O(n^3) :       n^3
+ O(2^n) :       2^n
+=> 2^10 , 2^log n, 3n + 100 log n, 4n , 4n log n + 2n, n log n ,n^2 + 10n , n^3 , 2^n
+*/
