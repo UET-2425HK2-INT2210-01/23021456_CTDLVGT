d76a8227d2ace9c5d3f6612dc6783e195a75e1b4
23021456 <23021456@vnu.edu.vn>
2025-03-21 13:40:36 +0700

    Update bai_1.cpp

--- Lect4_Assignments/bai_1.cpp
+++ Lect4_Assignments/bai_1.cpp
@@ -1,94 +1,11 @@
-#include <iostream>
-using namespace std;
-struct Node {
-    int data;
-    Node* next;
-    Node(int val) : data(val), next(nullptr) {}
-};
-class LinkedList {
-private:
-    Node* head;
-    Node* tail;
+/* Hai tính chất quan trọng của một hàm băm tốt là:
 
-public:
-    LinkedList() : head(nullptr), tail(nullptr) {}
-    void append(int x) {
-        Node* newNode = new Node(x);
-        if (head == nullptr) {
-            head = tail = newNode;
-        } else {
-            tail->next = newNode;
-            tail = newNode;
-        }
-        printList();
-    }
-    void search(int x) {
-        Node* current = head;
-        int position = 1;
-        bool found = false;
-        while (current != nullptr) {
-            if (current->data == x) {
-                found = true;
-                cout << position << endl;
-                break;
-            }
-            current = current->next;
-            position++;
-        }
-        if (!found) {
-            cout << "NO" << endl;
-        }
-    }
-    void reverse() {
-        Node* prev = nullptr;
-        Node* current = head;
-        Node* next = nullptr;
-        tail = head;
+Phân phối đồng đều (Giảm thiểu va chạm)
+Hàm băm cần phân phối các khóa (keys) một cách đồng đều trên bảng băm.
+Điều này giúp giảm thiểu va chạm (collision), tức là nhiều khóa cùng được ánh xạ vào một vị trí.
+Nếu phân phối không đều, sẽ xảy ra hiện tượng "dồn cục" (clustering), làm giảm hiệu suất tra cứu.
 
-        while (current != nullptr) {
-            next = current->next;
-            current->next = prev;
-            prev = current;
-            current = next;
-        }
-        head = prev;
-    }
-    void printList() {
-        Node* current = head;
-        while (current != nullptr) {
-            cout << current->data;
-            if (current->next != nullptr) cout << " ";
-            current = current->next;
-        }
-        cout << endl;
-    }
-    ~LinkedList() {
-        Node* current = head;
-        while (current != nullptr) {
-            Node* next = current->next;
-            delete current;
-            current = next;
-        }
-    }
-};
-int main() {
-    LinkedList list;
-    int n;
-    cin >> n; 
-    for (int i = 0; i < n; i++) {
-        string command;
-        cin >> command;
-        if (command == "append") {
-            int x;
-            cin >> x;
-            list.append(x);
-        } else if (command == "search") {
-            int x;
-            cin >> x;
-            list.search(x);
-        } else if (command == "reverse") {
-            list.reverse();
-        }
-    }
-    return 0;
-}
+Tính xác định (Nhất quán)
+Hàm băm phải luôn cho ra cùng một giá trị băm khi nhập vào cùng một khóa.
+Điều này đảm bảo các thao tác tìm kiếm, chèn, xóa hoạt động đúng.
+Nếu hàm băm thay đổi giá trị cho cùng một đầu vào, việc truy xuất dữ liệu sẽ bị lỗi. */
