3e8d1934db31dbba3d7a1fd87f0fdb88b969a414
23021456 <23021456@vnu.edu.vn>
2025-03-21 13:41:58 +0700

    Update bai_2.cpp

--- Lect4_Assignments/bai_2.cpp
+++ Lect4_Assignments/bai_2.cpp
@@ -1,91 +1,42 @@
-#include<iostream>
-using namespace std;
+/*Hàm băm: h(i) = (21 + 5) mod 11
+i           h(2i + 5) mod 11        
+12          7
+44          5
+13          9
+88          5
+23          7
+94          6
+11          5
+39          6
+20          1
+16          4
+5           4
 
-struct Node{
-	int data1;
-	int data2;
-	Node* next;
-	Node* prev;
-	Node(int x, int p){
-		data1 = x;
-		data2 = p;
-		next = nullptr;
-		prev = nullptr;
-		
-	}
-};
-class doublyLinkedList{
-	public:
-		Node* head = nullptr;
-		Node* tail = nullptr;
-		
-	void print(){
-		Node* temp = head;
-		while(temp != nullptr){
-			 cout << "(" << temp->data1 << "," << temp->data2 <<")";
-			 temp = temp -> next;
-			 if(temp != nullptr){
-			 	cout << ";";
-			 }
-		}
-		cout << endl;
-	}
-	void enqueue(int x, int p){
-		Node* newNode = new Node(x,p);
-		if(head == nullptr){
-			head = newNode;
-			tail = newNode;
-		}else if(p > head->data2){
-			newNode ->next = head;
-			head -> prev = newNode;
-			head = newNode;
-		}else if( p <= tail->data2){
-			tail -> next = newNode;
-			newNode -> prev = tail;
-			tail = newNode;
-		}else{
-			Node* temp = head;
-			while(temp -> next && p <= temp -> next -> data2){
-				temp = temp -> next;
-			}
-			 newNode->next = temp->next;
-   			 if (temp->next) {
-       		 temp->next->prev = newNode;
-   			 }
-  			  temp->next = newNode;
-   			 newNode->prev = temp;
-		}
-	}
-	void dequeue(){
-		if(head != nullptr){
-			Node *temp = head;
-			head = head -> next;
-			if(head != nullptr){
-				head -> prev = nullptr;
-			}else{
-				tail = nullptr;
-			}
-		delete temp;
-		}
-	}
-};
-int main(){
-	int n;
-	cin >> n;
-	int x,p;
-	doublyLinkedList myList;
-	string operat;
-	for(int i = 0; i < n; i++){
-		cin >> operat;
-		if(operat == "enqueue"){
-			cin >> x >> p;
-			myList.enqueue(x,p);
-		}else if(operat == "dequeue"){
-			myList.dequeue();
-		}else{
-			cout <<" invalid";
-		}
-	}
-	myList.print();
-	return 0;
-}
+Insert vào bảng
+a. Separate chaining
+
+0.
+1.  20 
+2.  
+3.
+4.  16 -> 5
+5.  44 -> 88 -> 11
+6.  94 -> 39
+7.  12 -> 23
+8.
+9.  13
+10
+
+b.Linear probing
+
+0.  11
+1.  39
+2.  20
+3.  5
+4.  16
+5.  44
+6.  88
+7.  12 
+8.  23
+9.  13
+10. 94*/
